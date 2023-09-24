9fc925a772367870e790a02a292ae38574da0251
PhoenixOleg <metall_2000@mail.ru>
2023-09-24 15:36:48 +0300

    Создание комментов для исходных "шаблонов" классов. Ушел думать над структурой классов

--- Module07/Program.cs
+++ Module07/Program.cs
@@ -7,38 +7,59 @@ namespace Module07
         static void Main(string[] args)
         {
 
+            Console.ReadKey();
         }
     }
 
+    /// <summary>
+    /// Абстрактный класс доставки "вообще". Базовый для всех видов доставок
+    /// </summary>
     abstract class Delivery
     {
         public string Address;
     }
 
+    /// <summary>
+    /// Доставка на дом. Наследуется от Delivery
+    /// </summary>
     class HomeDelivery : Delivery
+
     {
         /* ... */
     }
 
+    /// <summary>
+    /// Доставка в пункт выдачи (самовывоз типа 1). Наследуется от Delivery
+    /// </summary>
     class PickPointDelivery : Delivery
     {
         /* ... */
     }
 
+    /// <summary>
+    /// Доставка в маазин (самовывоз типа 2). Наследуется от Delivery
+    /// </summary>
     class ShopDelivery : Delivery
     {
         /* ... */
     }
 
-    class Order<TDelivery,
-    TStruct> where TDelivery : Delivery
+    /// <summary>
+    /// Класс заказа
+    /// </summary>
+    /// <typeparam name="TDelivery">Входной обобщенный (универсальный) параметр вида доставки. Ограничивается абстрактным классом Delivery</typeparam>
+    /// <typeparam name="TStruct">Входной обобщенный (универсальный) параметр структуры заказа. Ничем не огранивается. Возможно, далее сделаю ограничение struct или конкретной структурой</typeparam>
+    class Order<TDelivery, TStruct> where TDelivery : Delivery
     {
-        public TDelivery Delivery;
+        public TDelivery Delivery; //Поле доставки
 
-        public int Number;
+        public int Number; //Поле номера заказа
 
-        public string Description;
+        public string Description; //Поле описания заказа
 
+        /// <summary>
+        /// Метод отображения адреса доставки (получения) заказа
+        /// </summary>
         public void DisplayAddress()
         {
             Console.WriteLine(Delivery.Address);
