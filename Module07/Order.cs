2b5940cc09d9b77fccc4629b362c400e2a99a4ae
PhoenixOleg <metall_2000@mail.ru>
2023-10-03 21:31:46 +0300

    Перенос заготвки класса Order в отдельный файл. Отказ от классов оплаты. Тут скорее признаки. Особенно, по наличке

---
+++ Module07/Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module07
+{
+    /// <summary>
+    /// Класс заказа
+    /// </summary>
+    /// <typeparam name="TDelivery">Входной обобщенный (универсальный) параметр вида доставки. Ограничивается абстрактным классом Delivery</typeparam>
+    /// <typeparam name="TStruct">Входной обобщенный (универсальный) параметр структуры заказа. Ничем не огранивается. Возможно, далее сделаю ограничение struct или конкретной структурой</typeparam>
+    class Order<TDelivery, TStruct> where TDelivery : Delivery
+    {
+        public TDelivery Delivery; //Поле доставки
+
+        public int Number; //Поле номера заказа
+
+        public string Description; //Поле описания заказа
+
+        /// <summary>
+        /// Метод отображения адреса доставки (получения) заказа
+        /// </summary>
+        public void DisplayAddress()
+        {
+            Console.WriteLine(Delivery.Address);
+        }
+
+        // ... Другие поля
+    }
+}
